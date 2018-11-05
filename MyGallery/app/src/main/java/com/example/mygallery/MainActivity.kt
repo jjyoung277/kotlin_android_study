e5890ede6d98b89d26cc0ee7ef36064b52cec11a
unknown <wnsdud45880@knu.ac.kr>
2018-11-05 21:09:36 +0900

    갤러리 슬라이드

---
+++ MyGallery/app/src/main/java/com/example/mygallery/MainActivity.kt
@@ -0,0 +1,85 @@
+package com.example.mygallery
+
+import android.Manifest
+import android.content.pm.PackageManager
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.MediaStore
+import android.support.v4.app.ActivityCompat
+import android.support.v4.app.Fragment
+import android.support.v4.content.ContextCompat
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.alert
+import org.jetbrains.anko.noButton
+import org.jetbrains.anko.toast
+import org.jetbrains.anko.yesButton
+import kotlin.concurrent.timer
+
+class MainActivity : AppCompatActivity() {
+    private val REQUEST_READ_EXTERNAL_STORAGE = 1000
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            if(ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                alert("사진 정보를 얻으려면 외부 저장소 권한이 필수로 필요합니다", "권한이 필요한 이유") {
+                    yesButton {
+                        ActivityCompat.requestPermissions(this@MainActivity, arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
+                            REQUEST_READ_EXTERNAL_STORAGE)
+                    }
+                    noButton{ }
+                }.show()
+            } else {
+                ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE), REQUEST_READ_EXTERNAL_STORAGE)
+            }
+        } else {
+            getAllPhotos()
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when(requestCode) {
+            REQUEST_READ_EXTERNAL_STORAGE   ->  {
+                if((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                    getAllPhotos()
+                } else {
+                    toast("권한 거부 됨")
+                }
+                return
+            }
+        }
+    }
+
+    private fun getAllPhotos() {
+        val cursor = contentResolver.query(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, null,
+            null, null, MediaStore.Images.ImageColumns.DATE_TAKEN + " DESC")
+
+        val fragments = ArrayList<Fragment>()
+        if(cursor != null) {
+            while(cursor.moveToNext()) {
+                var uri = cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA))
+                Log.d("MainActivity", uri)
+                fragments.add(PhotoFragment.newInstance(uri))
+            }
+            cursor.close()
+        }
+
+        val adapter = MyPagerAdapter(supportFragmentManager)
+        adapter.updateFragments(fragments)
+        viewPager.adapter = adapter
+
+        timer(period = 3000) {
+            runOnUiThread {
+                if(viewPager.currentItem < adapter.count - 1) {
+                    viewPager.currentItem = viewPager.currentItem + 1
+                } else {
+                    viewPager.currentItem = 0
+                }
+            }
+        }
+    }
+}
---
+++ MyGallery/app/src/main/java/com/example/mygallery/MyPagerAdapter.kt
@@ -0,0 +1,20 @@
+package com.example.mygallery
+
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentStatePagerAdapter
+
+class MyPagerAdapter(fm: FragmentManager?) : FragmentStatePagerAdapter(fm) {
+    private val items = ArrayList<Fragment>()
+    override fun getItem(p0: Int): Fragment {
+        return items[p0]
+    }
+
+    override fun getCount(): Int {
+        return items.size
+    }
+
+    fun updateFragments(items: List<Fragment>) {
+        this.items.addAll(items)
+    }
+}
